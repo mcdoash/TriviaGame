025b386308d42f665f54903a5bfd57b356eb6cb5
mcdoash <mcdoash@users.noreply.github.com>
2019-09-27 20:22:29 -0400

    Add options, styling, font

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//loadTest function loads a test from the pre-existing 6 defined in triva.js
+function loadTest() {
+    //get test number
+    var testNum = document.forms["premade-test"]["premade-options"].value;
+    
+    //remove create test screen and show test screen 
+    var createScreen = document.getElementById("create-test");
+    var testScreen = document.getElementById("play-test");
+    
+    createScreen.style.display = "none";
+    testScreen.style.display = "block";
+}
