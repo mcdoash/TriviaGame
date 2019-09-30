c38e8e6baab92d8a54d48ab77fc6467ee602c4d8
mcdoash <mcdoash@users.noreply.github.com>
2019-09-29 23:51:07 -0400

    Form validation, clear test

--- script.js
+++ script.js
@@ -12,36 +12,32 @@ function setOption(selected) {
     document.getElementById("test-selected").innerHTML = selected.textContent;
 }
 
+let testNum = "";
 
 /*
-loadTest function loads a test from the pre-existing 6 defined in triva.js
+Loads a test from the pre-existing 6 defined in triva.js
 */
 function loadTest() {
     //get test number
-    let testNum = document.getElementById("test-selected").textContent;
-    console.log(document.getElementById("test-selected").textContent);
+    testNum = document.getElementById("test-selected").textContent;
     
     if(!testNum) {
         alert("Please select a test to load.");
         return;
-    }
-    if(testNum.localeCompare("Random")) {
-        let keys = Object.keys(tests);
-        testNum = keys[Math.floor(Math.random() * 6)];
     }
     
     document.getElementById("test-title").textContent = testNum;
     
     
     let test = tests[testNum];
-    console.log(test);
     let html = "";
     let q;
 
-    for(let i=0; i<=test.length-1; i++) {
+    for(let i=0; i<test.length; i++) {
         q = test[i];
-        html += '<div class="question"><h3>' + q.question + '<span class="tooltip"><p>Category: ' + q.category + '<br/>Difficulty: ' + q.difficulty + '</span></h3>';
+        html += '<div class="question" id="q' + i + '"><h3>' + q.question + '<span class="tooltip"><p>Category: ' + q.category + '<br/>Difficulty: ' + q.difficulty + '</span></h3>';
 
+        //ADD RANDOM FEATURE
         let options = [];
         for(let x=0; x<q.incorrect_answers.length; x++) {
             options.push(q.incorrect_answers[x]);
@@ -65,17 +61,168 @@ function loadTest() {
 }
 
 
+/*
+Checks each question to determine if all the questions were answered, and determine if they were right or wrong. Displays styling based on those answers
+*/
 function checkTest() {
-    if(!allAnswered) {
+    clearStyling();
+    
+    let allAnswered = true;
+    let test = tests[testNum];
+    let numCorrect = 0;
+    let correct = [];
+    let incorrect = [];
+    
+    //iterate through each question
+    for(let i=0; i<test.length; i++) {
+        let options = document.getElementsByName(i);
+        let qAnswered = false;
+        //iterate through each option in question
+        for(let x=0; x<options.length; x++) {
+            if(options[x].checked) {
+                
+                    qAnswered = true;
+                if(options[x].value.localeCompare(test[i].correct_answer) === 0) {
+                    correct.push(options[x]);
+                    numCorrect++;
+                }
+                else {
+                    incorrect.push(options[x]);
+                }
+            }
+        }
+        
+        if(qAnswered == false) {
+            allAnswered = false;
+
+            //get the assoicated question div and add unaswered class
+            let question = document.getElementById("q" + i);
+            question.classList.add("unanswered");
+        }
+    }
+    //if not all questions were answered, show alert
+    if(allAnswered == false) {
         //set unanswered q's text colour to red
         alert("Please answer all questions.");
         return;
     }
+    //if all answers were answered, we can  show styling for graded test
+    else {
+        //set styling for all correct answers
+        for(let i=0; i<correct.length; i++) {
+            correctAnswer(correct[i]);
+        }
+        //set styling for all incorrect answers
+        for(let i=0; i<incorrect.length; i++) {
+            incorrectAnswer(incorrect[i]);
+        }
+        //disable all options
+        for(let i=0; i<test.length; i++) {
+            let options = document.getElementsByName(i);
+            //diable each option in question
+            for(let x=0; x<options.length; x++) {
+                options[x].disabled = true;
+            }
+        }
+        //disable checking test
+        let checkBtn = document.getElementById("checkbtn");
+        checkBtn.removeAttribute("onclick");
+        checkBtn.classList.add("disabled");
+    }
+}
 
-    //reset unselected/incorrect/correct styling
-    //calc num correct
-    //add checkmark/styling to correct
-    //add x/styling to incorrect
+
+/*
+Adds styling for correct answers, and span with checkmark/correct designation
+*/
+function correctAnswer(selected) {
+    //gets the assoicated question div
+    let question = selected.parentElement.parentElement;
+    
+    question.classList.add("correct");
+    
+    let feedbackBar = document.createElement("span");
+    feedbackBar.setAttribute("class", "status");
+    
+    let check = document.createElement("i");
+    check.classList.add("fas", "fa-check-circle");
+    let text = document.createTextNode(" Correct!");
+    
+    feedbackBar.appendChild(check);
+    feedbackBar.appendChild(text);
+    question.insertBefore(feedbackBar, question.childNodes[0]);
+}
+
+
+/*
+Adds styling for incorrect answers, and span with checkmark/incorrect designation
+*/
+function incorrectAnswer(selected) {
+    //gets the assoicated question div
+    let question = selected.parentElement.parentElement;
+    
+    question.classList.add("incorrect");
+    
+    let feedbackBar = document.createElement("span");
+    feedbackBar.setAttribute("class", "status");
+    
+    let x = document.createElement("i");
+    x.classList.add("fas", "fa-times-circle");
+    let text = document.createTextNode(" Incorrect!");
+    
+    feedbackBar.appendChild(x);
+    feedbackBar.appendChild(text);
+    question.insertBefore(feedbackBar, question.childNodes[0]);
+}
+
+
+/*
+Clears all answers from the test 
+*/
+function clearTest() {
+    //ask user 
+    
+    //clear question styling 
+    clearStyling();
+    
+    //enable check test button
+        document.getElementById("checkbtn").setAttribute("onclick", "checkTest()");
+        document.getElementById("checkbtn").classList.remove("disabled");
+    
+    //enable and unselect all options
+    for(let i=0; i<test.length; i++) {
+        let options = document.getElementsByName(i);
+        //diable each option in question
+        for(let x=0; x<options.length; x++) {
+            options[x].disabled = false;
+            options[x].checked = false;
+        }
+    }
+}
+
+
+/*
+Clears styling for incorrect, correct, and unasnwered questions, therefore setting it to the "defaul" state
+@param      feedback    true if feebackBars should be removed
+                        false if it shoulf not
+*/
+function clearStyling() {
+    let questions = document.getElementsByClassName("question");
+    let feedbackBar = document.getElementsByClassName("status");
+    
+    for(let i=0; i<questions.length; i++) {
+        
+        questions[i].classList.remove("incorrect", "correct", "unanswered");
+        
+        //removes span with check/x mark, if there is one
+       if(feedbackBar.length > 0){
+           try {
+               questions[i].removeChild(feedbackBar[i]);
+           } catch (e) {
+               //Catch Statement
+           }
+       }
+    }
 }
 /*
 function sendPost(){
